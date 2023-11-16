b240fb99b987fc1145daa82b96a5bf139b1a0fe1
MatheusgMoreira <matheusmoreira1324@hotmail.com>
2023-11-16 18:55:47 -0300

    Atualização código

--- docs/codigo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogPessoal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
